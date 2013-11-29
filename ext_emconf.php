541ae4671fce02f5126e1d4bdcc23147a3afd576
sfsmfc <sfsmfc@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-29 12:41:34 +0000

    initial release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mfc_author/trunk@81796 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "mfc_author".
+ *
+ * Auto generated 29-11-2013 13:36
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Author',
+	'description' => 'Gives you the possibility to add author informations into every page or tx_news entry',
+	'category' => 'Marketing Factory',
+	'shy' => 0,
+	'version' => '1.2.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => 'pages,tx_news_domain_model_news',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Frank Kroeber, Simon Schmidt',
+	'author_email' => 'typo3@marketing-factory.de',
+	'author_company' => 'Marketing Factory Consulting GmbH',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.3.0-5.3.99',
+			'typo3' => '4.5.1-4.5.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+			'news' => '2.1.0-2.9.99',
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"3527";s:14:"ext_tables.php";s:4:"257b";s:14:"ext_tables.sql";s:4:"b1f9";s:41:"Configuration/TCA/tx_mfcauthor_author.php";s:4:"4d85";s:38:"Configuration/TypoScript/constants.txt";s:4:"c803";s:34:"Configuration/TypoScript/setup.txt";s:4:"da2a";s:44:"Resources/Private/Language/locallang_tca.xml";s:4:"c76f";s:34:"Resources/Public/Icon/fe_users.gif";s:4:"d178";s:43:"Resources/Public/Stylesheets/mfc_author.css";s:4:"f55c";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ ext_tables.sql
@@ -0,0 +1,42 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+	tx_mfcauthor_author varchar(100) DEFAULT '' NOT NULL
+);
+
+#
+# Table structure for table 'tx_news_domain_model_news'
+#
+CREATE TABLE tx_news_domain_model_news (
+	tx_mfcauthor_author  int(4) unsigned NOT NULL DEFAULT '0'
+);
+
+#
+# Table structure for table 'tx_mfcauthor_author'
+#
+CREATE TABLE tx_mfcauthor_author (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l18n_parent int(11) DEFAULT '0' NOT NULL,
+	l18n_diffsource mediumblob NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	firstname tinytext NOT NULL,
+	name tinytext NOT NULL,
+	email tinytext NOT NULL,
+	profil_link text NOT NULL,
+	mfc_author_image text,
+	description text,
+	twitter text,
+	xing text,
+	facebook text,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
