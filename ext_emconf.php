537ae073b5770a806552ae2f2a1292fd659605be
sfsmfc <sfsmfc@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-12-04 17:59:06 +0000

    raise version to 1.3.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mfc_author/trunk@81906 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "mfc_author".
  *
- * Auto generated 29-11-2013 13:36
+ * Auto generated 04-12-2013 18:58
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Gives you the possibility to add author informations into every page or tx_news entry',
 	'category' => 'Marketing Factory',
 	'shy' => 0,
-	'version' => '1.2.0',
+	'version' => '1.3.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'tt_news' => '3.3.0-3.9.99',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"3527";s:14:"ext_tables.php";s:4:"257b";s:14:"ext_tables.sql";s:4:"b1f9";s:41:"Configuration/TCA/tx_mfcauthor_author.php";s:4:"4d85";s:38:"Configuration/TypoScript/constants.txt";s:4:"c803";s:34:"Configuration/TypoScript/setup.txt";s:4:"da2a";s:44:"Resources/Private/Language/locallang_tca.xml";s:4:"c76f";s:34:"Resources/Public/Icon/fe_users.gif";s:4:"d178";s:43:"Resources/Public/Stylesheets/mfc_author.css";s:4:"f55c";}',
+	'_md5_values_when_last_written' => 'a:24:{s:12:"ext_icon.gif";s:4:"3527";s:14:"ext_tables.php";s:4:"4e07";s:14:"ext_tables.sql";s:4:"bb3b";s:41:"Configuration/TCA/tx_mfcauthor_author.php";s:4:"4d85";s:38:"Configuration/TypoScript/constants.txt";s:4:"c803";s:34:"Configuration/TypoScript/setup.txt";s:4:"7a5b";s:24:"Documentation/Images.txt";s:4:"8410";s:26:"Documentation/Includes.txt";s:4:"bae2";s:23:"Documentation/Index.rst";s:4:"850d";s:26:"Documentation/Settings.yml";s:4:"755c";s:32:"Documentation/Backend/Images.txt";s:4:"9396";s:31:"Documentation/Backend/Index.rst";s:4:"1704";s:37:"Documentation/Configuration/Index.rst";s:4:"77f5";s:27:"Documentation/FAQ/Index.rst";s:4:"e497";s:35:"Documentation/Images/add_author.png";s:4:"f1de";s:39:"Documentation/Images/author_example.png";s:4:"10fd";s:43:"Documentation/Images/background_devider.gif";s:4:"94bd";s:35:"Documentation/Images/logo_typo3.png";s:4:"f048";s:38:"Documentation/Images/select_author.png";s:4:"5f51";s:37:"Documentation/Introduction/Images.txt";s:4:"77aa";s:36:"Documentation/Introduction/Index.rst";s:4:"4390";s:44:"Resources/Private/Language/locallang_tca.xml";s:4:"c76f";s:34:"Resources/Public/Icon/fe_users.gif";s:4:"d178";s:43:"Resources/Public/Stylesheets/mfc_author.css";s:4:"f55c";}',
 	'suggests' => array(
 	),
 );
